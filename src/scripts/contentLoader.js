16d9caac1c4446e73285952774e4480b9812c614
Tony Borisov <tony.d.borisov@gmail.com>
2018-03-14 19:29:12 +0200

    script revised according to the changes of the content IDs

--- src/scripts/contentLoader.js
+++ src/scripts/contentLoader.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     // TODO: Switch to Home in prod
-    $("#content-wrap").load("content.html #contact");
+    $("#content-wrap").load("content.html #contact-page");
 
     $("#navigation").on("click", "li", function() {
         let page = $( this ).children().attr('href');
-        let target = "content.html " + page;
+        let target = "content.html " + page + "-page";
         $("#content-wrap").load(target);
     });
 });
 
 $(document).on('click', '#contact-button', function() {
-    $("#content-wrap").load("content.html #contact");
+    $("#content-wrap").load("content.html #contact-page");
 });
